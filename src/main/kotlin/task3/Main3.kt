85001713dfbf4e2e05d4fba737f1188402d287a5
Raddik251 <raddik@inbox.ru>
2022-07-17 21:35:41 +0300

    Add programm for Task3

---
+++ src/main/kotlin/task3/Main3.kt
@@ -0,0 +1,37 @@
+package task3
+
+fun main() {
+
+    var purchaseFinal = 0
+
+    println("Вы совершаете покупки каждый месяц? (Да/Нет)")
+
+    var melomanCheck = readLine()?.toString() ?: return
+
+    println("Ведите сумму покупки:")
+
+    var purchase = readLine()?.toInt() ?: return
+
+    if (melomanCheck == "Да") {
+        if (purchase <= 1_000_00) {
+            purchaseFinal = (purchase * 99) / 100
+        } else if (purchase >= 1_001_00 && purchase <= 10_000_00) {
+            purchaseFinal = ((purchase - 100_00) * 99) / 100
+        } else {
+            purchaseFinal = (((purchase * 95) / 100) * 99) / 100
+        }
+    } else {
+        if (purchase <= 1_000_00) {
+            purchaseFinal = purchase
+        } else if (purchase >= 1_001_00 && purchase <= 10_000_00) {
+            purchaseFinal = purchase - 100_00
+        } else {
+            purchaseFinal = (purchase * 95) / 100
+        }
+    }
+
+    println("Первоначальная сумма покупки: ${purchase / 100} рублей ${purchase % 100} копеек")
+    println("Скидка: ${(purchase - purchaseFinal) / 100} рублей ${(purchase - purchaseFinal) % 100} копеек")
+    println("Итоговая сумма покупки: ${purchaseFinal / 100} рублей ${purchaseFinal % 100} копеек")
+
+}
